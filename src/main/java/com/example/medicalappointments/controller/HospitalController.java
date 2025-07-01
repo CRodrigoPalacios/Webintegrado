233c211a2c52464090af49c9be46205176ab6eef
CarlosPalacios <144638763+CRodrigoPalacios@users.noreply.github.com>
2025-07-01 08:20:07 -0500

    fix crear hospitales

--- src/main/java/com/example/medicalappointments/controller/HospitalController.java
+++ src/main/java/com/example/medicalappointments/controller/HospitalController.java
@@ -5,6 +5,8 @@ import com.example.medicalappointments.repository.HospitalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class HospitalController {
     @GetMapping
     public List<Hospital> getAllHospitals() {
         return hospitalRepository.findAll();
+    }
+
+    @PostMapping
+    public Hospital createHospital(@RequestBody Hospital hospital) {
+        return hospitalRepository.save(hospital);
     }
 }
