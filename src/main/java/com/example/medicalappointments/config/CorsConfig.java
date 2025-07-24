5ca78a0beb25998785b4eba9f7e6001546016231
Swicher1 <97643962+Swicher1@users.noreply.github.com>
2025-07-23 23:44:03 -0500

    fix conexion

--- src/main/java/com/example/medicalappointments/config/CorsConfig.java
+++ src/main/java/com/example/medicalappointments/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.example.medicalappointments.config;
 
 import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,7 +14,9 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("https://webintegrado.vercel.app"));
+        config.setAllowedOrigins(Arrays.asList(
+            "https://webintegrado.vercel.app"
+        ));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setExposedHeaders(Arrays.asList("Authorization"));
